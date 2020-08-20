17aaf6d0b5beff0e03a1e4242e60a4cf7297cd2e
gegy1000 <gegy1000@gmail.com>
2020-08-20 10:21:22 +0200

    don't allow buying bow power unless you have one

--- src/main/java/supercoder79/wavedefense/game/WdItemShop.java
+++ src/main/java/supercoder79/wavedefense/game/WdItemShop.java
@@ -10,7 +10,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
-import net.minecraft.util.Formatting;
 import xyz.nucleoid.plasmid.shop.Cost;
 import xyz.nucleoid.plasmid.shop.ShopEntry;
 import xyz.nucleoid.plasmid.shop.ShopUi;
@@ -48,15 +47,14 @@ public final class WdItemShop {
                     })
             );
 
+            boolean hasBow = player.inventory.contains(new ItemStack(Items.BOW));
+
             int powerLevel = game.getEnchantmentLevel(game.powerLevels, player);
-            Cost powerCost = powerLevel >= 5 ? Cost.no() : Cost.ofIron((int) (Math.pow(2, powerLevel) * 16));
+            Cost powerCost = !hasBow || powerLevel >= 5 ? Cost.no() : Cost.ofIron((int) (Math.pow(2, powerLevel) * 16));
 
             shop.add(ShopEntry.ofIcon(Items.CROSSBOW)
                     .withName(new LiteralText("Bow Power " + (powerLevel + 1)))
                     .addLore(new LiteralText("Increases the power level of your bow."))
-                    // TODO: bows get power when you buy them, fixing this
-                    .addLore(new LiteralText("WARNING: You must buy a bow first").formatted(Formatting.RED))
-                    .addLore(new LiteralText("otherwise you will run into bugs!").formatted(Formatting.RED))
                     .withCost(powerCost)
                     .onBuy(p -> {
                         game.increaseEnchantment(game.powerLevels, player);
